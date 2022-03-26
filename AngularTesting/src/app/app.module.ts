f720b3ba2c1877f390ddbfd57d4e4c4de4178ca8
Cristopher Bohol <cris.bacus6@gmail.com>
2022-03-26 10:21:29 +0800

    MainActivity

--- AngularTesting/src/app/app.module.ts
+++ AngularTesting/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Calculator } from './calculator.services/calculator.services.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Calculator.ServicesComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
---
+++ AngularTesting/src/app/services/calculator.service.spec.ts
@@ -0,0 +1,25 @@
+import { CalculatorService } from './calculator.services';
+import { LoggerService } from './logger.service';
+
+describe('CalculatorService', () => {
+    it('should add 2 numbers', () => {
+        let calculator = new CalculatorService(new LoggerService());
+        let result = calculator.add(2, 2);
+        expect(result).toBe(4);
+    });
+    it('should subtract 2 numbers', () => {
+        let calculator = new CalculatorService(new LoggerService());
+        let result = calculator.subtract(2, 2);
+        expect(result).toBe(0);
+    });
+    it('should divide 2 numbers', () => {
+        let calculator = new CalculatorService(new LoggerService());
+        let result = calculator.divide(2, 2);
+        expect(result).toBe(1);
+    });
+    it('should multiply 2 numbers', () => {
+        let calculator = new CalculatorService(new LoggerService());
+        let result = calculator.multiply(2, 2);
+        expect(result).toBe(4);
+    });
+});
