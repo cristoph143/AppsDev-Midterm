d26b10a61b6844c64bb1853e5caa8ff7d864aa25
Cristopher Bohol <cris.bacus6@gmail.com>
2022-04-02 16:18:52 +0800

    baseline

--- AngularTesting/src/app/services/calculator.services.ts
+++ AngularTesting/src/app/services/calculator.services.ts
@@ -14,11 +14,36 @@ export class CalculatorService {
         return n1 - n2;
     }
     divide(n1: number, n2: number) {
-        this.logger.log('Subtraction operation called');
-        return n1 / n2;
+        let result;
+        this.logger.log('Division operation called');
+        if (n2 == 0) {
+            result = 'Cannot divide by 0';
+            this.logger.errorMessage('Check divide function -- cannot divide by 0');
+        } else {
+            result = n1 / n2;
+        }
+
+        return result;
     }
+
     multiply(n1: number, n2: number) {
+        let result = n1 * n2;
+
         this.logger.log('Subtraction operation called');
+
+        if (result < 0) {
+            this.logger.warn('Result is negative');
+        } else {
+            this.logger.log('Result is positive');
+        }
         return n1 * n2;
+    }
+
+    testFunction(x: number) {
+        if (x == 1) {
+            return 1;
+        } else if (x == 2) {
+            return 2;
+        } else return 3;
     }
 }
--- AngularTesting/src/app/services/logger.service.ts
+++ AngularTesting/src/app/services/logger.service.ts
@@ -6,4 +6,12 @@ export class LoggerService {
     log(message: string) {
         console.log(message);
     }
+
+    warn(message: string) {
+        console.warn(message);
+    }
+
+    errorMessage(message: string) {
+        console.error(message);
+    }
 }
